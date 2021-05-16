c6f97939b35ba5a64380a5cab35acce7c547adb6
tsimaphei <49167548+tsimaphei@users.noreply.github.com>
2021-05-16 23:23:12 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import {Route} from "react-router-dom";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
+import Login from './components/Login/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+            <div className='app-wrapper'>
+                <HeaderContainer />
+                <Navbar />
+                <div className='app-wrapper-content'>
+                    <Route path='/dialogs'
+                           render={ () => <DialogsContainer /> }/>
+
+                    <Route path='/profile/:userId?'
+                           render={ () => <ProfileContainer /> }/>
+
+                    <Route path='/users'
+                           render={ () => <UsersContainer /> }/>
+
+                    <Route path='/login~'
+                           render={ () => <Login /> }/>
+
+
+                </div>
+            </div>
+        )
 }
 
 export default App;
