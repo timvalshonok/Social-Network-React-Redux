872c175bdaf192a595b08b487a930a8051f41d03
tsimaphei <tima.valshonok@gmail.com>
2021-06-21 13:50:27 +0300

    78 ex

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,42 +3,50 @@ import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 import { Redirect } from 'react-router';
+import { Field, reduxForm } from 'redux-form';
+import AddMessageForm from './AddMessageForm/AddMessageForm';
 
 const Dialogs = (props) => {
 
     let state = props.dialogsPage;
 
-    let dialogsElements = state.dialogs.map( d => <DialogItem name={d.name} key={d.id} id={d.id} />  );
-    let messagesElements = state.messages.map( m => <Message message={m.message} key={m.id} /> );
+    let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} key={d.id} id={d.id} />);
+    let messagesElements = state.messages.map(m => <Message message={m.message} key={m.id} />);
     let newMessageBody = state.newMessageBody;
 
-    let onSendMessageClick = () => {
-        props.sendMessage();
+
+    let addNewMessage = (values) => {
+        alert();
+        props.sendMessage(values.newMessageBody);
     }
 
-    let onNewMessageChange = (e) => {
-        let body = e.target.value;
-        props.updateNewMessageBody(body);
-    }
-
-    if (!props.isAuth === false) return <Redirect to={"/login"}/>;
+    if (!props.isAuth === false) return <Redirect to={"/login"} />;
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                { dialogsElements }
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <div>{ messagesElements }</div>
-                <div>
-                    <div><textarea value={newMessageBody}
-                                   onChange={onNewMessageChange}
-                                   placeholder='Enter your message'></textarea></div>
-                    <div><button onClick={onSendMessageClick}>Send</button></div>
-                </div>
+                <div>{messagesElements}</div>
+
             </div>
+            <AddMessageForm onSubmit={addNewMessage}/>
         </div>
     )
 }
+
+// const AddMessageForm = (props) => {
+//     return (
+//         <form onSubmit={props.handleSubmit}>
+//                 <div>
+//                     <Field component='textarea' name='newMessageBody' placeholder='Enter your message' />
+//                 </div>
+//                    <div><button>Send</button></div>
+//         </form>
+//     )
+// }
+
+// const AddMessageFormRedux = reduxForm({form: 'dialogAddMessageForm'}) (AddMessageForm);
 
 export default Dialogs;
--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -3,13 +3,13 @@ import s from './ProfileInfo.module.css';
 
 
 class ProfileStatus extends React.Component {
+
     state = {
         editMode: false,
-        title: 'Yo'
+        status: this.props.status
     }
 
     activateEditMode = () => {
-        console.log("this:", this);
         this.setState({
             editMode: true
         });
@@ -18,6 +18,21 @@ class ProfileStatus extends React.Component {
         this.setState({
             editMode: false
         });
+        this.props.updateStatus(this.state.status);
+    }
+    onStatusChange = (e) => {
+        this.setState({
+            status: e.currentTarget.value
+        });
+       
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status != this.props.status) {
+            this.setState({
+                status: this.props.status
+            });
+        }
     }
 
     render() {
@@ -25,12 +40,12 @@ class ProfileStatus extends React.Component {
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                        <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status || "---"}</span>
                     </div>
                 }
                 {this.state.editMode &&
                     <div>
-                        <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status} />
+                        <input onChange={this.onStatusChange} autoFocus={true} onBlur={this.deactivateEditMode} value={this.state.status} />
                     </div>
                 }
             </div>
